b0ed88e05084205c1f80594db3e6b2558f7c3e83
Bishal Hasan Bhuiyan <bishalqx680@gmail.com>
2025-09-06 20:18:27 +0600

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 import json
 from time import time
-from pyrogram import Client, __version__ as __pyroversion__
+
+from pyrogram import Client, __version__ as __pyroVersion__
+from pyrogram.types import LinkPreviewOptions
+from pyrogram.enums import ClientPlatform
+
 from .utils.logger import setup_logging
 from config import CONFIG
 
@@ -16,10 +20,13 @@ ubot = Client(
     name="account",
     api_id=config.api_id,
     api_hash=config.api_hash,
-    app_version=f"Pyrogram {__pyroversion__}",
-    device_model="MSI b650m-p pro",
-    system_version="Windows 11",
-    session_string=config.session_string
+    app_version=f"{__pyroVersion__} x64",
+    device_model="Desktop",
+    system_version="Windows 11 x64",
+    session_string=config.session_string,
+    workdir="sys",
+    client_platform=ClientPlatform.DESKTOP,
+    link_preview_options=LinkPreviewOptions(is_disabled=True)
 )
 
 logger.info(f"""
@@ -31,6 +38,6 @@ Developed by
   \/_____/   \/_/   \/_____/   \/_/\/_/   \/_/\/_/   \/_____/ 
    
     Version: {__version__}
-    Library: kurigram {__pyroversion__}
+    Library: kurigram {__pyroVersion__}
     GitHub: https://github.com/bishalqx980
 """)
